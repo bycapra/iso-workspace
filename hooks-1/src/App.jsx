dc89926813230b28a36e8db025642f3e5c55d3a5
bycapra <ism.ysr@gmail.com>
2025-08-03 23:31:39 +0300

    - Sıfırlama

--- hooks-1/src/App.jsx
+++ hooks-1/src/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const setCounter = (type) =>{
-    if (type=='+')
-        setCount(count + 1)
-    else if(type=="-"){
-      let value = (count -1 > 0) ? count -1 : 0
+  const setCounter = (type) => {
+    if (type == "+") setCount(count + 1);
+    else if (type == "-") {
+      let value = count - 1 > 0 ? count - 1 : 0;
+
       setCount(value);
-    }        
-    else
-      throw "Hatalı parametre! Parametre Degeri: " + type;
-  }
+    } else if (type == null || type === "0") setCount(0);
+    else throw "Hatalı parametre! Parametre Degeri: " + type;
+  };
 
   return (
     <>
+      <div>Hooks - Test | Counter = {count}</div>
       <div>
-        Hooks - Test | Counter = {count}
+        <button
+          onClick={() => {
+            setCounter("+");
+          }}
+        >
+          Artır (+)
+        </button>
+        <button
+          onClick={() => {
+            setCounter("-");
+          }}
+        >
+          Azalt (-)
+        </button>
       </div>
       <div>
-        <button onClick={()=>{setCounter('+')}}>Artır (+)</button>
-        <button onClick={()=>{setCounter('-')}}>Azalt (-)</button>
+        <button
+          onClick={() => {
+            setCounter("0");
+          }}
+        >
+          Sıfırla
+        </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
