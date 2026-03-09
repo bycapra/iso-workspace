bd92b47c69aa4fb94e7c5db274be507a9a6a5c65
bycapra <ism.ysr@gmail.com>
2026-03-09 13:34:09 +0300

    - Email login tamamlandı - Google Login bölümüne başlanacak

--- firebase/src/components/Navbar.jsx
+++ firebase/src/components/Navbar.jsx
@@ -1,10 +1,23 @@
+import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../Firebase'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
 
 function Navbar() {
+
+  const navigator = useNavigate();
+
+  const logout = async () => {
+    var result  =await signOut(auth);
+    toast.success("Oturum sonlandırıldı.");
+    navigator('/auth');
+  }
+
   return (
     <div className='navbar'>
       <div className='navbar-left'>Navbar</div>
-      <div className='navbar-right'>Çıkış Yap</div>
+      <div className='navbar-right' onClick={logout}>Çıkış Yap</div>
     </div>
   )
 }
--- firebase/src/pages/Auth.jsx
+++ firebase/src/pages/Auth.jsx
@@ -1,21 +1,77 @@
-import React from 'react'
+import React, { use, useState } from "react";
 import { FaGoogle } from "react-icons/fa";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword  } from "firebase/auth";
+import { toast } from "react-toastify";
+import { auth } from "../Firebase";
+import { useNavigate } from "react-router-dom";
 
 function Auth() {
-  return (
-    <div className='auth'>
-        <h3 className='auth-header'>Giriş Yap / Kayıt ol </h3>
-      <div className='input-div'>
-        <input type='text' placeholder='Email adresinizi giriniz'/> 
-        <input type='password' placeholder='parolanızı girin'/>
-      </div>
-      <div>
-        <button className='google-button'><FaGoogle style={{marginTop:"2px"}} /> Google ile Giriş Yap</button>
-        <button className='login-button'>Giriş Yap</button>
-        <button className='register-button'>Kaydol</button>
-      </div>
-    </div>
-  )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigator = useNavigate();
+
+  const register = async () => {
+    try {
+      
+      const registerResult = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      const user = registerResult.user;
+
+      if (user) {
+        toast.success("Kullanıcı başarıyla oluşturuldu");
+        setEmail("");
+        setPassword("");
+      }
+    } catch (e) {
+      toast.error("Hata oluştu: " + e.message);
+    }
+  };
+
+const login = async ()=>{
+  try{
+     var loginResponse = await signInWithEmailAndPassword(auth, email, password);
+     if(loginResponse.user){
+      toast.success("Kullanıcı oturumu başarıyla açıldı");
+      navigator('/');
+     }
+  }catch(e){
+    toast.error("Oturum Açılamadı: " + e.message)
+  }
 }
 
-export default Auth
+
+  return (
+    <div className="auth">
+      <h3 className="auth-header">Giriş Yap / Kayıt ol </h3>
+      <div className="input-div">
+        <input
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email adresinizi giriniz"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="parolanızı girin"
+        />
+      </div>
+      <div>
+        <button className="google-button">
+          <FaGoogle style={{ marginTop: "2px" }} /> Google ile Giriş Yap
+        </button>
+        <button className="login-button" onClick={login}>Giriş Yap</button>
+        <button className="register-button" onClick={register}>
+          Kaydol
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Auth;
